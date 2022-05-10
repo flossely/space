61ef03189bc5a65342b1cd8e879473d1666abfdc
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-10 23:41:00 +0000

    Changed paradigm

--- alt.php
+++
@@ -1,5 +0,0 @@
-<?php
-$alt = [];
-for ($i = $yi; $i >= $yo; $i--) {
-    include $i.'.row';
-}
--- spread.php
+++
@@ -1,7 +0,0 @@
-<?php
-$yi = ($_REQUEST['yi']) ? $_REQUEST['yi'] : 90;
-$yo = ($_REQUEST['yo']) ? $_REQUEST['yo'] : -90;
-for ($i = $yi; $i >= $yo; $i--) {
-    copy('row.php', $i.'.row');
-    chmod($i.'.row', 0777);
-}

8a25d3c18ab2d9dce7dc84f9483d11de3e02e96b
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-19 04:40:56 +0000

    Added new models and functions

--- machine.php
+++ machine.php
@@ -1,28 +1,29 @@
 <?php
 if ($sub == $obj) {
     if ($subRating >= 0) {
-        $subAct = rand(0,1);
+        $subAct = rand(0, 1);
         if ($subAct == 0) {
             echo $sub.' ('.$subRating.') is thinking...<br>';
         } elseif ($subAct == 1) {
-            $arc = ['d','r','u','l'];
-            $arcRand = rand(0,count($arc)-1);
-            if (file_exists($sub.'/foot.'.$arc[$arcRand].'.png')) {
-                echo $sub." has opened a <a href='".$sub."/foot.".$arc[$arcRand].".png'>brothel</a>";
-            } else {
-                echo $sub.' ('.$subRating.') is thinking...<br>';
-            }
+            include 'move.php';
         }
     } elseif ($subRating < 0) {
         echo $sub.' ('.$subRating.') is dead<br>';
     }
 } elseif ($sub != $obj) {
     if ($subRating >= 0) {
-        include 'discover.php';
-        if (($distX <= $subRating) && ($distY <= $subRating) && ($distZ <= $subRating)) {
-            include 'discovered.php';
-        } else {
-            echo $sub.': '.$obj.' not reachable<br>';
+        $subAct = rand(0, 2);
+        if ($subAct == 0) {
+            include 'discover.php';
+            if (($distX <= $subRating) && ($distY <= $subRating) && ($distZ <= $subRating)) {
+                include 'discovered.php';
+            } else {
+                echo $sub.': '.$obj.' not reachable<br>';
+            }
+        } elseif ($subAct == 1) {
+            include 'move.php';
+        } elseif ($subAct == 2) {
+            echo $sub.' ('.$subRating.') is thinking...<br>';
         }
     } elseif ($subRating < 0) {
         echo $sub.' ('.$subRating.') is dead<br>';
---
+++ move.php
@@ -0,0 +1,18 @@
+<?php
+$subStepArr = [2, 4, 8, 12, 24, 48];
+$subStepVelo = $subStepArr[rand(0, 5)];
+$subStep = round(($subRating / $subStepVelo), 0) + 2;
+$subDirect = rand(0, 5);
+if ($subDirect == 0) {
+    $subX = $subX + $subStep;
+} elseif ($subDirect == 1) {
+    $subX = $subX - $subStep;
+} elseif ($subDirect == 2) {
+    $subY = $subY + $subStep;
+} elseif ($subDirect == 3) {
+    $subY = $subY - $subStep;
+} elseif ($subDirect == 4) {
+    $subZ = $subZ + $subStep;
+} elseif ($subDirect == 5) {
+    $subZ = $subZ - $subStep;
+}

eca62b29ebc39678de15ef27a3018f89db713adf
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-28 06:14:48 +0000

    Add

--- force.php
+++ force.php
@@ -1,11 +1,11 @@
 <?php
 if ($subMode > $objMode) {
     $forceDiff = $subMode - $objMode;
-    $subForce = round(($forceDiff * 2), 0);
+    $subForce = round(($forceDiff * 4), 0);
 } elseif ($subMode < $objMode) {
     $forceDiff = $objMode - $subMode;
-    $subForce = round(($forceDiff * 1), 0);
+    $subForce = round(($forceDiff * 2), 0);
 } elseif ($subMode == $objMode) {
     $forceDiff = $subMode - $objMode;
-    $subForce = round(($forceDiff / 2), 0);
+    $subForce = round(($forceDiff / 4), 0);
 }

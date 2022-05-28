f9cd45283e54c166774ffccf2d324c0a27c02ee6
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-28 07:07:48 +0000

    Add

--- autosubsave.php
+++ autosubsave.php
@@ -1,8 +1,8 @@
 <?php
 exec('chmod -R 777 .');
+file_put_contents($sub.'/coord', $subX.';'.$subY.';'.$subZ);
+chmod($sub.'/coord', 0777);
 file_put_contents($sub.'/rating', $subRating);
 chmod($sub.'/rating', 0777);
 file_put_contents($sub.'/mode', $subMode);
 chmod($sub.'/mode', 0777);
-file_put_contents($sub.'/coord', $subX.';'.$subY.';'.$subZ);
-chmod($sub.'/coord', 0777);

2a69ad67e0c710c0e29143cca16ec3e8f4789922
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-28 07:00:20 +0000

    Add

--- autoobjsave.php
+++ autoobjsave.php
@@ -1,8 +1,8 @@
 <?php
 exec('chmod -R 777 .');
+file_put_contents($obj.'/coord', $objX.';'.$objY.';'.$objZ);
+chmod($obj.'/coord', 0777);
 file_put_contents($obj.'/rating', $objRating);
 chmod($obj.'/rating', 0777);
 file_put_contents($obj.'/mode', $objMode);
 chmod($obj.'/mode', 0777);
-file_put_contents($obj.'/coord', $objX.';'.$objY.';'.$objZ);
-chmod($obj.'/coord', 0777);

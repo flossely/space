9a172807c44f167d8bc38c011ec5f3ade701d47b
Karl August Nestor <jesuscaesar@outlook.com>
2022-07-13 07:06:37 +0000

    Latest update

--- machine.php
+++ machine.php
@@ -1,4 +1,130 @@
 <?php
+
+function numgen($min,$max) {
+    return ($min+lcg_value()*(abs($max-$min)));
+}
+
+function alive($r) {
+    return ($r >= 0);
+}
+
+function dead($r) {
+    return ($r < 0);
+}
+
+function valuable($r) {
+    return ($r > 0);
+}
+
+function invaluable($r) {
+    return ($r <= 0);
+}
+
+function dist($ix,$iy,$iz,$jx,$jy,$jz) {
+    if ($ix > $jx) {
+        $dix = $ix - $jx;
+    } elseif ($ix < $jx) {
+        $dix = $jx - $ix;
+    } elseif ($ix == $jx) {
+        $dix = 0;
+    }
+    if ($iy > $jy) {
+        $diy = $iy - $jy;
+    } elseif ($iy < $jy) {
+        $diy = $jy - $iy;
+    } elseif ($iy == $jy) {
+        $diy = 0;
+    }
+    if ($iz > $jz) {
+        $diz = $iz - $jz;
+    } elseif ($iz < $jz) {
+        $diz = $jz - $iz;
+    } elseif ($iz == $jz) {
+        $diz = 0;
+    }
+    
+    return sqrt($dix ** 2 + $diy ** 2 + $diz ** 2);
+}
+
+function reach($ix,$iy,$iz,$jx,$jy,$jz,$f) {
+    if ($ix > $jx) {
+        $dix = $ix - $jx;
+    } elseif ($ix < $jx) {
+        $dix = $jx - $ix;
+    } elseif ($ix == $jx) {
+        $dix = 0;
+    }
+    if ($iy > $jy) {
+        $diy = $iy - $jy;
+    } elseif ($iy < $jy) {
+        $diy = $jy - $iy;
+    } elseif ($iy == $jy) {
+        $diy = 0;
+    }
+    if ($iz > $jz) {
+        $diz = $iz - $jz;
+    } elseif ($iz < $jz) {
+        $diz = $jz - $iz;
+    } elseif ($iz == $jz) {
+        $diz = 0;
+    }
+    
+    return (($dix <= $f) && ($diy <= $f) && ($diz <= $f));
+}
+
+function relate($sm, $om) {
+    if (($sm == 0 && $om == 0) || ($sm > 0 && $om < 0) || ($sm < 0 && $om > 0)) {
+        $status = 'foe';
+    } elseif (($subMode > 0 && $objMode > 0) || ($subMode < 0 && $objMode < 0) || ($subMode > 0 && $objMode == 0) || ($subMode < 0 && $objMode == 0) || ($subMode == 0 && $objMode > 0) || ($subMode == 0 && $objMode < 0)) {
+        $status = 'friend';
+    }
+    
+    return $status;
+}
+
+if (file_exists("locale")) {
+    $localeOpen = file_get_contents("locale");
+    $locale = ($localeOpen != "") ? $localeOpen : "en";
+} else {
+    $locale = "en";
+}
+
+$lingua = $locale;
+
+$navi =
+[
+    'en' =>
+    [
+        'back' => 'back',
+        'dead' => 'dead',
+        'down' => 'down',
+        'forward' => 'forward',
+        'heal' => 'heal',
+        'hit' => 'hit',
+        'left' => 'left',
+        'right' => 'right',
+        'standby' => 'stand by',
+        'strike' => 'strike',
+        'tract' => 'traced',
+        'up' => 'up',
+    ],
+    'la' =>
+    [
+        'back' => 'ad tergum',
+        'dead' => 'mortuus',
+        'down' => 'ad deorsum',
+        'forward' => 'ad deinceps',
+        'heal' => 'sano',
+        'hit' => 'ferio',
+        'left' => 'ad sinistram',
+        'right' => 'ad dextram',
+        'standby' => 'adsistit',
+        'strike' => 'pulso',
+        'tract' => 'vestigo',
+        'up' => 'ad sursum',
+    ],
+];
+
 $subRand = rand(0,$last);
 $subID = $subRand;
 $sub = $list[$subRand];
@@ -50,81 +176,98 @@ if ($subRating <= 0) {
 }
 
 if ($sub == $obj) {
-    if ($subRating >= 0) {
-        echo '@'.$sub.'('.$subRating.'){'.$subX.';'.$subY.';'.$subZ.'} = @'.$obj.'('.$objRating.'){'.$objX.';'.$objY.';'.$objZ.'}<br>';
-    } elseif ($subRating < 0) {
-        echo '@'.$sub.'('.$subRating.') = @'.$obj.'('.$objRating.')<br>';
-    }
+    echo '@'.$sub.' ('.$subRating.') '.$navi[$lingua]['standby'].'<br>';
 } elseif ($sub != $obj) {
     if ($subRating >= 0) {
         if ($objRating >= 0) {
-            if ($subX > $objX) {
-    	        $distX = $subX - $objX;
-            } elseif ($subX < $objX) {
-                $distX = $objX - $subX;
-	    } elseif ($subX == $objX) {
-                $distX = 0;
-	    }
-	    if ($subY > $objY) {
-                $distY = $subY - $objY;
-	    } elseif ($subY < $objY) {
-                $distY = $objY - $subY;
-	    } elseif ($subY == $objY) {
-                $distY = 0;
-	    }
-	    if ($subZ > $objZ) {
-                $distZ = $subZ - $objZ;
-	    } elseif ($subZ < $objZ) {
-                $distZ = $objZ - $subZ;
-	    } elseif ($subZ == $objZ) {
-    	        $distZ = 0;
-	    }
-	    if (($distX <= $subForce) && ($distY <= $subForce) && ($distZ <= $subForce)) {
-	        if (($subMode == 0 && $objMode == 0) || ($subMode > 0 && $objMode < 0) || ($subMode < 0 && $objMode > 0)) {
-        	    $objRating = $objRating - $subForce;
-        	    $subRating = $subRating + $subForce;
-        	    echo '@'.$sub.'('.$subRating.') - @'.$obj.'('.$objRating.')<br>';
-    		} elseif (($subMode > 0 && $objMode > 0) || ($subMode < 0 && $objMode < 0) || ($subMode > 0 && $objMode == 0) || ($subMode < 0 && $objMode == 0) || ($subMode == 0 && $objMode > 0) || ($subMode == 0 && $objMode < 0)) {
-        	    $objRating = $objRating + $subForce;
-        	    $subRating = $subRating - $subForce;
-        	    echo '@'.$sub.'('.$subRating.') + @'.$obj.'('.$objRating.')<br>';
+	    if (reach($subX,$subY,$subZ,$objX,$objY,$objZ,$subForce)) {
+	        if (relate($subMode, $objMode) == 'foe') {
+	            $subAction = rand(0, 2);
+	            if ($subAction == 0) {
+        	        $subX = $objX;
+        	        $subY = $objY;
+        	        $subZ = $objZ;
+        	        echo '@'.$sub.' '.$navi[$lingua]['tract'].' ('.dist($subX,$subY,$subZ,$objX,$objY,$objZ).' @'.$obj.'<br>';
+	            } elseif ($subAction == 1) {
+	                $subX = $objX;
+        	        $subY = $objY;
+        	        $subZ = $objZ;
+	                $objRating -= $subForce;
+        	        $subRating += $subForce;
+        	        echo '@'.$sub.' ('.$subRating.') '.$navi[$lingua]['hit'].' ('.$subForce.') @'.$obj.' ('.$objRating.')<br>';
+        	    } elseif ($subAction == 2) {
+	                $objRating -= $subForce;
+        	        $subRating += $subForce;
+        	        echo '@'.$sub.' ('.$subRating.') '.$navi[$lingua]['strike'].' ('.$subForce.') @'.$obj.' ('.$objRating.')<br>';
+	            }
+    		} elseif (relate($subMode, $objMode) == 'friend') {
+    		    $subAction = rand(0, 2);
+	            if ($subAction == 0) {
+	                $subX = $objX;
+        	        $subY = $objY;
+        	        $subZ = $objZ;
+        	        echo '@'.$sub.' '.$navi[$lingua]['tract'].' ('.dist($subX,$subY,$subZ,$objX,$objY,$objZ).' @'.$obj.'<br>';
+	            } elseif ($subAction == 1) {
+	                $subX = $objX;
+        	        $subY = $objY;
+        	        $subZ = $objZ;
+	                $objRating += $subForce;
+        	        $subRating -= $subForce;
+        	        echo '@'.$sub.' ('.$subRating.') '.$navi[$lingua]['heal'].' ('.$subForce.') @'.$obj.' ('.$objRating.')<br>';
+	            } elseif ($subAction == 2) {
+	                $objRating += $subForce;
+        	        $subRating -= $subForce;
+        	        echo '@'.$sub.' ('.$subRating.') '.$navi[$lingua]['heal'].' ('.$subForce.') @'.$obj.' ('.$objRating.')<br>';
+	            }
     		}
 	    } else {
 	        $subDirect = rand(0, 5);
+	        $subMove = numgen(0, $subForce);
 		if ($subDirect == 0) {
-    	    	    $subX = $subX + $subForce;
+    	    	    $subX = $subX + $subMove;
+    	    	    echo '@'.$sub.' '.$navi[$lingua]['right'].' ('.$subMove.')<br>';
 		} elseif ($subDirect == 1) {
-    	    	    $subX = $subX - $subForce;
+    	    	    $subX = $subX - $subMove;
+    	    	    echo '@'.$sub.' '.$navi[$lingua]['left'].' ('.$subMove.')<br>';
         	} elseif ($subDirect == 2) {
-            	    $subY = $subY + $subForce;
+            	    $subY = $subY + $subMove;
+            	    echo '@'.$sub.' '.$navi[$lingua]['forward'].' ('.$subMove.')<br>';
         	} elseif ($subDirect == 3) {
-            	    $subY = $subY - $subForce;
+            	    $subY = $subY - $subMove;
+            	    echo '@'.$sub.' '.$navi[$lingua]['back'].' ('.$subMove.')<br>';
         	} elseif ($subDirect == 4) {
-            	    $subZ = $subZ + $subForce;
+            	    $subZ = $subZ + $subMove;
+            	    echo '@'.$sub.' '.$navi[$lingua]['up'].' ('.$subMove.')<br>';
 		} elseif ($subDirect == 5) {
-            	    $subZ = $subZ - $subForce;
+            	    $subZ = $subZ - $subMove;
+            	    echo '@'.$sub.' '.$navi[$lingua]['down'].' ('.$subMove.')<br>';
 		}
-		echo '@'.$sub.'('.$subRating.'){'.$subX.';'.$subY.';'.$subZ.'}<br>';
 	    }
         } elseif ($objRating < 0) {
 	    $subDirect = rand(0, 5);
+	    $subMove = numgen(0, $subForce);
 	    if ($subDirect == 0) {
-    	    	$subX = $subX + $subForce;
+    	        $subX = $subX + $subMove;
+    	    	echo '@'.$sub.' '.$navi[$lingua]['right'].' ('.$subMove.')<br>';
             } elseif ($subDirect == 1) {
-    	    	$subX = $subX - $subForce;
+    	    	$subX = $subX - $subMove;
+    	    	echo '@'.$sub.' '.$navi[$lingua]['left'].' ('.$subMove.')<br>';
             } elseif ($subDirect == 2) {
-            	$subY = $subY + $subForce;
+            	$subY = $subY + $subMove;
+            	echo '@'.$sub.' '.$navi[$lingua]['forward'].' ('.$subMove.')<br>';
             } elseif ($subDirect == 3) {
-            	$subY = $subY - $subForce;
+            	$subY = $subY - $subMove;
+            	echo '@'.$sub.' '.$navi[$lingua]['back'].' ('.$subMove.')<br>';
             } elseif ($subDirect == 4) {
-            	$subZ = $subZ + $subForce;
+            	$subZ = $subZ + $subMove;
+            	echo '@'.$sub.' '.$navi[$lingua]['up'].' ('.$subMove.')<br>';
 	    } elseif ($subDirect == 5) {
-            	$subZ = $subZ - $subForce;
-            }
-	    echo '@'.$sub.'('.$subRating.'){'.$subX.';'.$subY.';'.$subZ.'}<br>';
+            	$subZ = $subZ - $subMove;
+            	echo '@'.$sub.' '.$navi[$lingua]['down'].' ('.$subMove.')<br>';
+	    }
         }
     } elseif ($subRating < 0) {
-        echo '@'.$sub.'('.$subRating.')<br>';
+        echo '@'.$sub.' ('.$subRating.') '.$navi[$lingua]['dead'].'<br>';
     }
 }
 
